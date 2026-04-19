c7294f0644f2ac4adec4cc6d4e35b710c925b671
MrRobot <david_d_93@hotmail.com>
2026-04-18 19:27:34 -0600

    Modified remote fetch paths to point to 'legacy/16.4' branch

--- ct/frigate.sh
+++ ct/frigate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://git.community-scripts.org/community-scripts/ProxmoxVE/raw/branch/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/MR-Research/ProxmoxVE/refs/heads/legacy/16.4/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Authors: MickLesk (CanbiZ) | Co-Author: remz1337
 # License: MIT | https://github.com/MR-Research/ProxmoxVE/raw/main/LICENSE
